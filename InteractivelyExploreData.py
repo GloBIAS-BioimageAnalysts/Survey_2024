cced89f48f6f26818abfa4cb67c4b4e35137e915
bethac07 <bcimini@broadinstitute.org>
2025-07-22 08:26:55 -0400

    First-pass marimo notebook
    Typoooos
    
    Correct folder, please

---
+++ InteractivelyExploreData.py
@@ -0,0 +1,72 @@
+import marimo
+
+__generated_with = "0.14.12"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import pandas
+    import pygwalker
+    return mo, pandas, pygwalker
+
+
+@app.cell
+def _(pandas):
+    df = pandas.read_csv("data/survey2024_headersandcommascleaned_recleaned.csv")
+    df.drop(columns=["Timestamp"],inplace=True)
+    return (df,)
+
+
+@app.cell
+def _(mo):
+    mo.md("""# Welcome to the data exploration page for the GloBIAS 2024 Survey""")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        """
+    Please read our preprint on [arXiv](https://arxiv.org/abs/2507.06407)
+
+    Want to play with the data yourself? Try a couple of tools below!
+    """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md("""## Explore or transform the dataframe itself in Marimo's dataframe explorer""")
+    return
+
+
+@app.cell
+def _(df, mo):
+    dataframe= mo.ui.dataframe(df)
+    dataframe
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        """
+    ## Explore, transform, or graph the data in Pygwalker
+
+    (If you're having trouble, check your aggregation and stacking settings, and try making sure "row count" is in your axes!)
+    """
+    )
+    return
+
+
+@app.cell
+def _(df, pygwalker):
+    pygwalker.walk(df)
+    return
+
+
+if __name__ == "__main__":
+    app.run()
